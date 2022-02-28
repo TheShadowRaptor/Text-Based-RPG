24970f50b5cb1c87244fd9d2df31c364ae89f849
Edmund Wohlmuth <edmundwoh@gmail.com>
2022-02-28 16:41:24 -0400

    Begun work on map.txt

---
+++ Text-Based-RPG/PowerUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Based_RPG
+{
+    class PowerUp : GameObject
+    {
+        public void Start(int x, int y)
+        {
+            objectX = x;
+            objectY = y;
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
